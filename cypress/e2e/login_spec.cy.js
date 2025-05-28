fc479f8f89cfd22453eb384f059fe0fdb947e6ea
vanbeo1234 <vbich18@gmail.com>
2025-05-28 15:11:13 +0700

    Check login fail #2

--- cypress/e2e/login_spec.cy.js
+++ cypress/e2e/login_spec.cy.js
@@ -9,4 +9,12 @@ describe('Login Test', () => {
     cy.url().should('include', '/inventory.html');
   });
 
+  // Kiểm thử: Hiển thị thông báo lỗi khi đăng nhập với thông tin không hợp lệ
+  it('Should show error message with invalid credentials', () => {
+    cy.visit('https://www.saucedemo.com');
+    cy.get('#user-name').type('invalid_user');
+    cy.get('#password').type('wrong_password');
+    cy.get('#login-button').click();
+    cy.get('[data-test="error"]').should('contain', 'Username and password do not match');
+  });
 });
