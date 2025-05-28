a3c8fc0a1f971e5e8fb839891b0b4fb77cd689dd
vanbeo1234 <vbich18@gmail.com>
2025-05-28 15:19:25 +0700

    Test the function of sorting products by price from low to high #4

---
+++ cypress/e2e/cart_spec.cy.js
@@ -0,0 +1,22 @@
+describe('Cart Test', () => {
+
+  // Trước mỗi test, truy cập trang login và đăng nhập với tài khoản hợp lệ
+  beforeEach(() => {
+    cy.visit('https://www.saucedemo.com');
+    cy.get('#user-name').type('standard_user');
+    cy.get('#password').type('secret_sauce');
+    cy.get('#login-button').click();
+  });
+
+   // Kiểm tra chức năng thêm sản phẩm vào giỏ hàng
+  it('Should add a product to the cart', () => {
+    cy.get('.inventory_item').first().find('button').contains('Add to cart').click();
+    cy.get('.shopping_cart_badge').should('have.text', '1');
+  });
+
+  // Kiểm tra chức năng sắp xếp sản phẩm theo giá từ thấp đến cao
+  it('Should sort products by price low to high', () => {
+    cy.get('.product_sort_container').select('lohi');
+    cy.get('.inventory_item_price').first().should('have.text', '$7.99');
+  });
+});
